c6e33f5211d4f07c775f9d81a6dc4db83eff5548
Suren <sharyan96@mail.ru>
2017-04-23 23:40:11 +0400

    solved problem with unicode symbols, added new util for that

---
+++ src/main/java/com/application/utils/ConverteSymbols.java
@@ -0,0 +1,32 @@
+package com.application.utils;
+
+/**
+ * Created by surik on 4/23/17
+ */
+public class ConverteSymbols {
+
+    private static StringBuilder buffer;
+    private static StringBuilder toConvert;
+
+    public static String converteFromHex(String textToConvert){
+        if (!textToConvert.matches(".*\\\\.*")) {
+            return textToConvert;
+        }
+        buffer = new StringBuilder("");
+        toConvert = new StringBuilder(textToConvert);
+        int index;
+            while((index = toConvert.indexOf("\\"))>= 0) {
+                buffer.append(toConvert.substring(0, index));
+
+                String convert = toConvert.substring(index, index + 6);
+                char cha = (char) Integer.parseInt(convert.substring(2), 16);
+
+                buffer.append(cha);
+
+                String notFixed = toConvert.substring(index+6);
+                toConvert.delete(0,toConvert.length()).append(notFixed);
+            }
+        buffer.append(toConvert);
+        return buffer.toString();
+    }
+}
