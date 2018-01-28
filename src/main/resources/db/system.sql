0d409a4e40d8bfa9a9d79c6cfc2042dc893ddf44
Surendev <sharyan96@mail.ru>
2018-01-28 18:24:10 +0400

    Update system.sql

--- src/main/resources/db/system.sql
+++ src/main/resources/db/system.sql
@@ -1,36 +1,75 @@
-DROP TABLE IF EXISTS students;
-DROP TABLE IF EXISTS questions;
 DROP TABLE IF EXISTS answers;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS topics;
+DROP TABLE IF EXISTS students_ratings;
+DROP TABLE IF EXISTS subjects;
 DROP TABLE IF EXISTS students;
+DROP TABLE IF EXISTS groups;
+
+CREATE TABLE groups
+(
+  id             INTEGER PRIMARY KEY NOT NULL,
+  group_name     TEXT                NOT NULL,
+  srudents_count INT                 NOT NULL,
+  srudents_email TEXT                NOT NULL
+);
+
+
 CREATE TABLE students
 (
-  id                 INTEGER PRIMARY KEY NOT NULL,
-  first_name         TEXT                NOT NULL,
-  last_name          TEXT                NOT NULL,
-  middle_name        TEXT                NOT NULL,
-  course             INT     DEFAULT 1 NOT NULL,
-  "group"            TEXT    DEFAULT '' NOT NULL
+  id          INTEGER PRIMARY KEY NOT NULL,
+  first_name  TEXT                NOT NULL,
+  last_name   TEXT                NOT NULL,
+  middle_name TEXT                NOT NULL,
+  course      INT DEFAULT 1       NOT NULL,
+  group_id    INT                 NOT NULL,
+  CONSTRAINT students_group FOREIGN KEY (group_id) REFERENCES groups (id)
+);
+
+CREATE TABLE subjects (
+  id     INTEGER PRIMARY KEY,
+  name   TEXT NOT NULL,
+  credit INT  NOT NULL
+);
+
+CREATE TABLE students_ratings
+(
+  id         INTEGER PRIMARY KEY NOT NULL,
+  student_id INT                 NOT NULL,
+  subject_id INT                 NOT NULL,
+  date       DATE                NOT NULL,
+  rating     INT                 NOT NULL,
+  CONSTRAINT to_student FOREIGN KEY (student_id) REFERENCES students (id),
+  CONSTRAINT to_subject FOREIGN KEY (subject_id) REFERENCES subjects (id)
+);
+
+CREATE TABLE topics (
+  id         INTEGER PRIMARY KEY,
+  topic_name TEXT NOT NULL,
+  subject_id INT  NOT NULL,
+  CONSTRAINT topic_to_subject FOREIGN KEY (subject_id) REFERENCES subjects (id)
+);
+
+
+CREATE TABLE questions
+(
+  id            INTEGER PRIMARY KEY    NOT NULL,
+  question_text TEXT                   NOT NULL,
+  rating        INTEGER                NOT NULL,
+  topic_id      INTEGER                NOT NULL,
+  answer_hash   VARCHAR(64)            NOT NULL,
+  CONSTRAINT question_to_topics FOREIGN KEY (topic_id) REFERENCES topics (id)
 );
 
 
 CREATE TABLE answers
 (
-  id          INTEGER PRIMARY KEY  AUTOINCREMENT,
-  text        TEXT    NOT NULL,
-  to_question INTEGER NOT NULL,
-  CONSTRAINT answers_to_question FOREIGN KEY (to_question) REFERENCES questions (id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE
+  id          INTEGER PRIMARY KEY,
+  answer_text TEXT    NOT NULL,
+  question_id INTEGER NOT NULL,
+  CONSTRAINT answers_to_question FOREIGN KEY (question_id) REFERENCES questions (id)
 );
 
-CREATE TABLE questions
-(
-  id       INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-  question TEXT                              NOT NULL,
-  rating   INTEGER                           NOT NULL,
-  topic    NVARCHAR(50)                      NOT NULL,
-  answer   VARCHAR(64)                       NOT NULL
-);
 CREATE INDEX questions_rating
   ON questions (rating);
 
